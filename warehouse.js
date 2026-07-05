2c92dd933d41e2ab2410cc81eb7fd812fd5d4aa8
diogo-tavares <78868918+diogo-tavares@users.noreply.github.com>
2026-07-05 20:43:37 +0100

    Update warehouse.js

--- warehouse.js
+++ warehouse.js
@@ -144,7 +144,7 @@
         html += '</table>';
         html += '</div>';
         html += '</td></tr></table>';
-        html += '<small><strong>Gestor de Armazéns</strong> - By rodaSbro</small>';
+        html += '<small><strong>Gestor de Armazéns</strong> - By rodaSbro </small>';
         html += '</div>';
 
         Dialog.show('warehouse_helper', html);
